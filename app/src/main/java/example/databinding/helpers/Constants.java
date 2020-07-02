f652b92359e8249e62f3a3b236794e1480f54927
Davidodari <davidkibzodari@gmail.com>
2020-07-02 22:29:09 +0300

    refactor data binding sample

---
+++ app/src/main/java/example/databinding/helpers/Constants.java
@@ -0,0 +1,13 @@
+package example.databinding.helpers;
+
+/**
+ * Contains constants used in the app.
+ * <p>
+ * Using enums for keys makes is easier to know what key to expect and removes errors brought about
+ * by writing the string constants yourself.
+ */
+public class Constants {
+    public enum Keys {
+        PRODUCT_DETAIL
+    }
+}
